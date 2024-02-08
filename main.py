a2db0276e60543a4a3b144184db761023449d351
tahiroVSC <a7740915@gmail.com>
2024-02-08 20:43:17 +0600

    commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Написать функцию, которая выводит на экран циклом пять строк из нулей, причем каждая строка должна быть пронумерована. 1
+# Дается 2 строки "Aidana" и "Adilet" . Вам нужно в результате получить смешанную строку из двух имен, буква за буквой. Результат: "AAiddialneat” 1
+# Напишите функцию, которая проверяет, сколько раз каждое слово в переданной фразе было использовано. Например: “Money, money, money, it’s always sunny, in the richmen’s world”. money: 3, it’s: 1, always: 1, sunny: 1, in: 1, the: 1, richmen’s: 1, world: 1.
+
+# def sobes():
+#         num = 0
+#         n = 0
+#         for i in range(5):
+#             n += 1
+#             print(f"{n}: {num}")
+# sobes()
+
+# name1 = "Aidana"
+# name2 = "Adilet"
+# print(name1 + name2[::2])
+
+# def abu():
+#     list = "Money, money, money, it's always sunny, in the richmen's world"
+#     list1 = list.lower()
+#     money = list1.count("money")
+#     its = list1.count("it's")
+#     always = list1.count("always")
+#     sunny =list1.count("sunny")
+#     inn = list1.count("in")
+#     the = list1.count("the")
+#     richmens = list1.count("richmen's")
+#     world = list1.count("world")
+#     print(f"money:{money}, its: {its}, always:{always}, sunny:{sunny}, in:{inn}, the:{the}, richmens:{richmens}, world: {world}")
+# abu()
